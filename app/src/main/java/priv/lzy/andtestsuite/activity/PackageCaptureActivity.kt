295bded9c797cf16d240e91c69d49ec960b117fb
zhiyuan <lizhiyuan7777@gmail.com>
2020-04-26 23:59:31 +0800

    feat(package capture): add capture activity

---
+++ app/src/main/java/priv/lzy/andtestsuite/activity/PackageCaptureActivity.kt
@@ -0,0 +1,13 @@
+package priv.lzy.andtestsuite.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import priv.lzy.andtestsuite.R
+
+class PackageCaptureActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_package_capture)
+    }
+}
