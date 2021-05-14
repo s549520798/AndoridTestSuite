0feb81dc8de7f27e73ef376f97ca0b59c0150025
Zhiyuan <lazylee7777@163.com>
2021-05-14 16:15:12 +0800

    change netbare module to java

--- app/src/main/java/priv/lzy/andtestsuite/ATSApplication.kt
+++ app/src/main/java/priv/lzy/andtestsuite/ATSApplication.kt
@@ -2,15 +2,29 @@ package priv.lzy.andtestsuite
 
 import android.app.Application
 import android.content.Context
+import com.github.megatronking.netbare.NetBare
 import priv.lzy.andtestsuite.utils.LogUtil
 
 class ATSApplication : Application() {
     private val mTag = "ATSApplication"
 
+    companion object {
+        const val JSK_ALIAS = "AndroidTestSuite"
+
+        private lateinit var sInstance: ATSApplication
+
+        fun getInstance(): Application {
+            return sInstance
+        }
+    }
 
     override fun onCreate() {
         super.onCreate()
         LogUtil.i(mTag, "onCreate")
+        sInstance = this
+
+        // 初始化NetBare
+        NetBare.get().attachApplication(this, BuildConfig.DEBUG)
     }
 
     override fun attachBaseContext(base: Context?) {
--- app/src/main/java/priv/lzy/andtestsuite/service/TestSuiteVpnService.kt
+++ app/src/main/java/priv/lzy/andtestsuite/service/TestSuiteVpnService.kt
@@ -1,11 +1,61 @@
 package priv.lzy.andtestsuite.service
 
-import android.net.VpnService
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import com.github.megatronking.netbare.NetBareService
+import priv.lzy.andtestsuite.ATSApplication
+import priv.lzy.andtestsuite.R
+import priv.lzy.andtestsuite.activity.PackageCaptureActivity
 import java.net.Socket
 
-class TestSuiteVpnService : VpnService() {
+class TestSuiteVpnService : NetBareService() {
+    companion object {
+        private val CHANNEL_ID = ATSApplication.getInstance().applicationContext.packageName + ".NOTIFICATION_CHANNEL_ID"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            if (notificationManager.getNotificationChannel(CHANNEL_ID) == null) {
+                notificationManager.createNotificationChannel(
+                    NotificationChannel(CHANNEL_ID,
+                    getString(R.string.app_name), NotificationManager.IMPORTANCE_LOW)
+                )
+            }
+        }
+    }
 
     override fun protect(socket: Socket?): Boolean {
         return super.protect(socket)
+    }
+
+    override fun notificationId(): Int {
+        return 100
+    }
+
+    override fun createNotification(): Notification {
+        val intent = Intent(this, PackageCaptureActivity::class.java)
+        intent.addCategory(Intent.CATEGORY_LAUNCHER)
+        intent.action = Intent.ACTION_MAIN
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent,
+            PendingIntent.FLAG_UPDATE_CURRENT)
+        //TODO 替换图标等信息
+        return NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(getString(R.string.app_name))
+            .setContentText(getString(R.string.app_name))
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher))
+            .setOngoing(true)
+            .setContentIntent(pendingIntent)
+            .build()
     }
 }
